b306d7bd68aabb1de95d93183a39e06658c62925
Jordan Mackie <jm354@st-andrews.ac.uk>
2019-02-15 14:57:29 +0000

    First commit

---
+++ src/main/java/dist/sys/pdilemma/PdilemmaApplication.java
@@ -0,0 +1,14 @@
+package dist.sys.pdilemma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdilemmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdilemmaApplication.class, args);
+	}
+
+}
+
