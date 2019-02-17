cfb5f24ad3d8b0869ade06adb23b1b53d445eb85
Jordan Mackie <jm354@st-andrews.ac.uk>
2019-02-17 19:07:06 +0000

    Add progress so far to report.

--- server/src/main/java/dist/sys/pdilemma/services/impl/RandomChoiceServiceImpl.java
+++ server/src/main/java/dist/sys/pdilemma/services/impl/RandomChoiceServiceImpl.java
@@ -2,10 +2,11 @@ package dist.sys.pdilemma.services.impl;
 
 import dist.sys.pdilemma.models.Choice;
 import dist.sys.pdilemma.services.RandomChoiceService;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Service;
 
 import java.util.Random;
 
+@Service
 public class RandomChoiceServiceImpl implements RandomChoiceService {
 
     private static final Random random = new Random();
