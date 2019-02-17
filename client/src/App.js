420b6297d266b7c85c3af2691899af4dca5e202b
Jordan Mackie <jm354@st-andrews.ac.uk>
2019-02-17 17:25:43 +0000

    Add logging to application

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {testConnection} from "./PersecutorService";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1>Press to Test</h1>
+                    <button onClick={() => testConnection()}>Test</button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
--- server/src/main/java/dist/sys/pdilemma/controllers/ProsecutorController.java
+++ server/src/main/java/dist/sys/pdilemma/controllers/ProsecutorController.java
@@ -1,6 +1,8 @@
 package dist.sys.pdilemma.controllers;
 
 import dist.sys.pdilemma.services.ProsecutorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/prosecutor")
 public class ProsecutorController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ProsecutorController.class);
+
     @Autowired
     private ProsecutorService prosecutorService;
 
     @RequestMapping("/test")
     public String testConnection() {
+        LOG.debug("Test connection reached");
         return prosecutorService.testConnection();
     }
 
