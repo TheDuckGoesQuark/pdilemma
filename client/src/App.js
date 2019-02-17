82594ab3e6ce290214596898e6a77f8ab37a9958
Jordan Mackie <jm354@st-andrews.ac.uk>
2019-02-17 17:33:54 +0000

    Add render of response text

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {responseText: ""}
+    }
+
+    handleTestConnection() {
+        testConnection()
+            .then(text => this.setState({responseText: text}));
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h1>Press to Test</h1>
-                    <button onClick={() => testConnection()}>Test</button>
+                    <button onClick={() => this.handleTestConnection()}>Test</button>
+                    <p>{this.state.responseText}</p>
                 </header>
             </div>
         );
