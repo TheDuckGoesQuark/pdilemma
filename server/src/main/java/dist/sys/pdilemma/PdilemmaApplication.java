be5f7ee14b41cdd205bea8e90463430afcf550be
Jordan Mackie <jmackie97@hotmail.com>
2019-02-23 21:19:51 +0000

    Add swagger ui docs

--- server/src/main/java/dist/sys/pdilemma/PdilemmaApplication.java
+++ server/src/main/java/dist/sys/pdilemma/PdilemmaApplication.java
@@ -2,12 +2,44 @@ package dist.sys.pdilemma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableSwagger2
 public class PdilemmaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PdilemmaApplication.class, args);
 	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("dist.sys.pdilemma"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo(
+				"Prisoners Dilemma API",
+				"RESTful API for starting and running prisoners dilemma games.",
+				"1.0",
+				null,
+				new Contact("150008022", null, null),
+				null, null, Collections.emptyList());
+	}
+
 }
 
